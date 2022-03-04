0f16f52e5dc18770d96232c3baf09eec42b8199d
kinofgl <kinofgl@gmail.com>
2022-03-04 17:47:11 +0800

    引用运行命令提供的参数

--- autobot.py
+++ autobot.py
@@ -9,7 +9,7 @@ import time
 import urllib.request
 import requests
 from Eduic.DailySentence import EduicContent
-from KEY.dingtalkbot_key import PiPiDou_webhook, PiPiDou_secret
+import sys
 from dingtalkchatbot.chatbot import DingtalkChatbot, ActionCard, FeedLink, CardItem
 
 # 定义钉钉机器人
@@ -183,9 +183,12 @@ md_message = '### 今天天气  \n  ' \
              '##### 每日一句：{sentence}  \n  '\
     .format(image=image, sentence=sentence)
 
+# 接收系统传入的webhook和secret
+pipidou_webhook = sys.argv[1]
+pipidou_secret = sys.argv[2]
 
 # 实例化DingtalkChatbot
-pipidou = DingtalkChatbot(PiPiDou_webhook, secret=PiPiDou_secret)
+pipidou = DingtalkChatbot(pipidou_webhook, secret=pipidou_secret)
 
 pipidou.send_text(msg='欢迎使用自动化测试脚本')
 pipidou.send_markdown(title="每日一句", text=md_message, is_at_all=False)
