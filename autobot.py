de98e13f701db2274c59728f4f642556143565e7
kinofgl <kinofgl@gmail.com>
2022-03-04 15:00:51 +0800

    添加了Eduic DailySentence模块

--- autobot.py
+++ autobot.py
@@ -8,7 +8,9 @@ import re
 import time
 import urllib.request
 import requests
-
+from Eduic.DailySentence import EduicContent
+from KEY.dingtalkbot_key import PiPiDou_webhook, PiPiDou_secret
+from dingtalkchatbot.chatbot import DingtalkChatbot, ActionCard, FeedLink, CardItem
 
 # 定义钉钉机器人
 class DingTalkWebHook(object):
@@ -168,3 +170,22 @@ if __name__ == '__main__':
     ding.send_message(my_makedown)
     # autoding = DingTalkWebHook(url=webhook, secret=secret)
     # autoding.send_message(mkd_msg)
+
+# 实例化Euic
+euic = EduicContent()
+# 取得每日内容
+sentence = euic.get_daily_content()[0]
+# 取得每日图片
+image = euic.get_daily_content()[1]
+
+md_message = '### 今天天气  \n  ' \
+             '![picture]({image})   \n  ' \
+             '##### 每日一句：{sentence}  \n  '\
+    .format(image=image, sentence=sentence)
+
+
+# 实例化DingtalkChatbot
+pipidou = DingtalkChatbot(PiPiDou_webhook, secret=PiPiDou_secret)
+
+pipidou.send_text(msg='欢迎使用自动化测试脚本')
+pipidou.send_markdown(title="每日一句", text=md_message, is_at_all=False)
