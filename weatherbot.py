f8b96fe1ead43f1169e9d831f940fcf6c868b303
kinofgl <kinofgl@gmail.com>
2022-03-14 11:09:20 +0800

    调整天气预售信息

--- weatherbot.py
+++ weatherbot.py
@@ -29,15 +29,14 @@ forcast = qWeather.get_weather_forecast()
 # 实例化Euic
 euic = EduicContent()
 # 取得每日内容
-sentence = euic.get_daily_content()[0]
+# sentence = euic.get_daily_content()[0]
 # 取得每日图片
 image = euic.get_daily_content()[1]
 # 编辑要发送的信息
-md_message = '### 今天天气  \n  ' \
+md_message = '### 北海实时天气  \n  ' \
              '##### {forcast}  \n  ' \
              '![picture]({image})  \n  ' \
-             '##### {sentence}  \n  '\
-    .format(forcast=forcast, image=image, sentence=sentence)
+    .format(forcast=forcast, image=image, )
 
 # 接收系统传入的webhook和secret
 pipidou_webhook = sys.argv[1]
@@ -47,6 +46,6 @@ pipidou_secret = sys.argv[2]
 pipidou = DingtalkChatbot(pipidou_webhook, secret=pipidou_secret)
 
 pipidou.send_text(msg='欢迎使用自动化测试脚本')
-pipidou.send_markdown(title="今天天气", text=md_message, is_at_all=False)
+pipidou.send_markdown(title="北海实时天气", text=md_message, is_at_all=False)
 # pipidou.send_markdown(title="工作信息", text=job_message, is_at_all=False)
 
