b6a586d4095fd7578cd507eb21ea7209f5493f55
kinofgl <kinofgl@gmail.com>
2022-03-15 08:55:47 +0800

    优化了脚本

--- weatherbot.py
+++ weatherbot.py
@@ -4,18 +4,6 @@ import sys
 from dingtalkchatbot.chatbot import DingtalkChatbot, ActionCard, FeedLink, CardItem
 from Eduic.DailySentence import EduicContent
 from qWeather.qWeatherAPI import QWeatherApi
-from Lib.BeiHaiJob import BeiHaiJob
-
-# # 实例化BeiHaiJob
-# Beihai = BeiHaiJob()
-# # 获取工作信息
-# job_info = Beihai.get_job_now()
-# # 整理成文字
-# job_message = ''
-# for item in job_info:
-#     for i in range(len(item)):
-#         job_message += item[i] + ', '
-#     job_message += '  \n  '
 
 # 取得天气信息
 # 取得系统传入KEY
@@ -33,19 +21,17 @@ euic = EduicContent()
 # 取得每日图片
 image = euic.get_daily_content()[1]
 # 编辑要发送的信息
-md_message = '### 北海实时天气  \n  ' \
+md_message = '#### 北海实时天气  \n  ' \
              '##### {forcast}  \n  ' \
              '![picture]({image})  \n  ' \
     .format(forcast=forcast, image=image, )
 
 # 接收系统传入的webhook和secret
-pipidou_webhook = sys.argv[1]
-pipidou_secret = sys.argv[2]
+bot_webhook = sys.argv[1]
+bot_secret = sys.argv[2]
 
 # 实例化DingtalkChatbot
-pipidou = DingtalkChatbot(pipidou_webhook, secret=pipidou_secret)
+bot = DingtalkChatbot(bot_webhook, secret=bot_secret)
 
-pipidou.send_text(msg='欢迎使用自动化测试脚本')
-pipidou.send_markdown(title="北海实时天气", text=md_message, is_at_all=False)
+bot.send_markdown(title="北海实时天气", text=md_message, is_at_all=False)
 # pipidou.send_markdown(title="工作信息", text=job_message, is_at_all=False)
-
