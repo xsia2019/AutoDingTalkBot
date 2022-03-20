67c67f062c93e6710f4fbd44bbd89f0b04faf65a
kinofgl <kinofgl@gmail.com>
2022-03-20 14:01:25 +0800

    修改文件名称为全小写字母

--- beihaijob.py
+++ beihaijob.py
@@ -3,6 +3,7 @@
 import sys
 from dingtalkchatbot.chatbot import DingtalkChatbot, ActionCard, FeedLink, CardItem
 from Lib.BeiHaiJob import BeiHaiJob
+from Lib.gxrcAPI import GXRCAPI
 # from KEY.dingtalkbot_key import PiPiDou_webhook, PiPiDou_secret
 
 if __name__ == '__main__':
@@ -19,10 +20,19 @@ if __name__ == '__main__':
         # 获取招聘信息
         beiHaiJob = BeiHaiJob()
         jobInfo = beiHaiJob.get_job_now(10000)
-        job_message = jobInfo[1]
+        bhjob_message = jobInfo[1]
+
+        # 获取gxrc招聘信息
+        gxrc = GXRCAPI()
+        gxrc_message = gxrc.get_job_message()
+
         # 发送消息
-        bot.send_markdown(title="北海招聘信息", text=job_message, is_at_all=False)
-        bot.send_markdown(title="Copyright", text='By beiHaiJob.py, AutoDingTalkBot  \n  ', is_at_all=False)
+        bot.send_markdown(title="北海招聘信息", text=bhjob_message, is_at_all=False)
+        bot.send_markdown(title="广西人才网", text=gxrc_message, is_at_all=False)
+
+        bot.send_markdown(title="Copyright", text='By beihaijob.py, AutoDingTalkBot  \n  ', is_at_all=False)
+
     except Exception as e:
         print(e)
         print('发送失败')
+
