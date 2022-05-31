7f1e064ee2a06dc61a7f5cf8172a2af6a2b7f281
kinofgl <kinofgl@gmail.com>
2022-05-31 16:26:02 +0800

    关闭生活指数信息。

--- weatherbot.py
+++ weatherbot.py
@@ -15,11 +15,11 @@ qWeather = QWeatherApi(qweather_key)
 # 取得天气信息
 forcast = qWeather.get_weather_forecast()
 # 取得生活指数
-indices = qWeather.get_my_indices('Beihai')
-# 舒适度指数
-indice_text = ''
-for i in range(len(indices)):
-    indice_text += indices[i] + '  \n  '
+# indices = qWeather.get_my_indices('Beihai')
+# # 舒适度指数
+# indice_text = ''
+# for i in range(len(indices)):
+#     indice_text += indices[i] + '  \n  '
 
 # 取得每日图片和每日一句
 # 实例化Eudic
@@ -34,9 +34,9 @@ md_message = '#### 北海实时天气  \n  ' \
              '![picture]({image})  \n  ' \
     .format(forcast=forcast, image=image, )
 
-indices_message = '#### 北海生活指数  \n  ' \
-                  '###### {indices}  \n  ' \
-    .format(forcast=forcast, indices=indice_text, )
+# indices_message = '#### 北海生活指数  \n  ' \
+#                   '###### {indices}  \n  ' \
+#     .format(forcast=forcast, indices=indice_text, )
 
 
 # 接收系统传入的webhook和secret
@@ -49,5 +49,4 @@ bot_secret = sys.argv[2]
 bot = DingtalkChatbot(bot_webhook, secret=bot_secret)
 
 bot.send_markdown(title="北海实时天气", text=md_message, is_at_all=False)
-bot.send_markdown(title="北海生活指数", text=indices_message, is_at_all=False)
-
+# bot.send_markdown(title="北海生活指数", text=indices_message, is_at_all=False)
